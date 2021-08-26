ffd0fb9e3c3d6c4e382be21c7148bf6a7fbd07d0
Isaac Abitey <abiteyisaac2902gmail.com>
2021-08-26 11:00:47 -0700

    adding 5-printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - printf this sentence "with proper grammar, but the outcome is a piece of art" and returns 0 if the compiled without errors
+ * */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
