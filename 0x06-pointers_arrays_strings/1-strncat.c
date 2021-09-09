15fd29b486076e7e3ee3a7169b347fe1122607db
Isaac Abitey <abiteyisaac2902gmail.com>
2021-09-09 04:30:20 -0700

    This program concatenats two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 /*
  * File: 1-strncat.c
- * Auth: Brennan D Baraban
+ * Auth: Isaac Abitey
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strncat - Concatenates two strings using at most
