ce517f68ac1fe23841ed203c61848c77618dc5bd
Abitey290 <84156920+Abitey290@users.noreply.github.com>
2021-09-16 19:23:05 +0000

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,24 @@
 #include "main.h"
 
+include <stdio.h>
+
+int wildcmp(char *, char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = wildcmp("school-school.c", "sch*ool.c");
+	printf("%d\n", r);
+	return (0);
+}
+
+
 int bandersnatch(char *s1, char *s2);
 char *move(char *s2);
 
